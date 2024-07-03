0baa356be88cc64d8b9b30a0ce0b8da5ea77f279
DawoudTormos <103008170+DawoudTormos@users.noreply.github.com>
2024-07-03 22:38:54 +0300

    Fixing filenameBug (capitalized name).

--- scripts/GameLevel.js
+++ scripts/GameLevel.js
@@ -33,7 +33,7 @@ class GameLevel extends Phaser.Scene {
       //to have all ressources in ram
 
 
-      this.load.image("tileset", "./assets/images/tileset.png");
+      this.load.image("tileset", "./assets/images/Tileset.png");
       this.load.image("background", "./assets/images/Ground.png");
 
 
